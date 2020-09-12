332022470cb6c2a7a18cab1161e6c9b340586a69
Herr doctor Pep <HerrDoctorPep@users.noreply.github.com>
2020-09-12 21:59:05 +0200

    Functional test now checks input of to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -14,17 +16,28 @@ class NewVisitorTest(unittest.TestCase):
         # Edith wants to check out a new to-do app 
         self.browser.get('http://localhost:8000')
 
-        # She notices the page title mentions to-do lists 
+        # She notices the page title and header mention to-do lists 
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do',header_text)
 
         # She is invited to enter a to do item right away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
 
         # She type 'buying peacock feathers' in the text box
+        inputbox.send_keys('Buy peacock feathers')
 
         # when she hits enter the page updates with one item 'Buy peacock feathers' as item in a list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
 
         # There is still a text box inviting her to enter another item
+        self.fail('Finish the test!')
 
         # She enters 'Use peacock feathers to make fly'
 
