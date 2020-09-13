1087243e8f332e420dc590e1a7b2f0ec8348d6f1
Herr doctor Pep <HerrDoctorPep@users.noreply.github.com>
2020-09-13 22:13:12 +0200

    Code refactoring: function th check for row in table

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self,row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text,[row.text for row in rows])
+
     def test_can_start_list_and_retreive_it_later(self):
 
         # Edith wants to check out a new to-do app 
@@ -32,9 +37,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to enter another item
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -46,10 +49,8 @@ class NewVisitorTest(unittest.TestCase):
 
        # Page updates again an shows both items on the list
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
-        self.assertIn('2: Use peacock feathersto makea fly',[row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathersto makea fly')
         
         # Edith wonders if the page will remember her lists. 
         # She sees that the page has generated a unique url
