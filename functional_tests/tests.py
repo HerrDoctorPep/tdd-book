1b47f521c1ec9b1b9a12d7ec73ff9b96aee58ed0
Herr doctor Pep <HerrDoctorPep@users.noreply.github.com>
2020-09-26 18:41:24 +0200

    New list in separate page

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -95,7 +95,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Francis starts a new list by entering a new item
 
-        inputbox = self.browser.find.element_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
