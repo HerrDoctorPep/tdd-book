72feb582008289c1ca6d80eb1e84ee0c054f0d56
Herr doctor Pep <HerrDoctorPep@users.noreply.github.com>
2020-11-08 15:32:40 +0100

    Bootstrap stling. FT still fails at test_can_start_list_for_one_user, test_layout_and_styling, and  test_multiple_users_can_start_lists_at_different_urls

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,11 +1,12 @@
-from django.test import LiveServerTestCase
+# from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 import time
 
 MAX_WAIT = 10
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -13,7 +14,7 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def wait_for_row_in_list_table(self,row_text):
+    def wait_for_row_in_gittable(self,row_text):
         start_time = time.time()
         while True:
             try:
